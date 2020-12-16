8fa182d740554f35703e0c756a42e9cc0e619467
RemyBen <pntdu83@gmail.com>
2020-12-16 11:57:56 +0100

    add navbar and AisShipType entity

---
+++ src/Entity/AisShipType.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AisShiptypeRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=AisShiptypeRepository::class)
+ */
+class AisShiptype
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=60)
+     */
+    private $libelle;
+
+    /**
+     * @ORM\Column(type="integer")
+     * @Assrt\Length(min=1,
+     *          max = 9,
+     *          minMessage = "Le type d'un navire est compris entre 1 et 9"
+     *          maxMessage = "Le type d'un navire est compris entre 1 et 9"
+     *          allowEmptyString = false
+     *          )
+     */
+    private $aisShipType;
+    
+    
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLibelle(): ?string
+    {
+        return $this->libelle;
+    }
+
+    public function setLibelle(string $libelle): self
+    {
+        $this->libelle = $libelle;
+
+        return $this;
+    }
+
+    public function getAisShipType(): ?int
+    {
+        return $this->aisShipType;
+    }
+
+    public function setAisShipType(int $aisShipType): self
+    {
+        try {
+            if ($this->aisShipType >= 1 && $this->aisShipType <= 9)
+            $this->aisShipType = $aisShipType;
+            return $this;
+            
+        } catch (Exception $ex) {
+            return $ex;
+        }
+    }
+}
